4630a00280643a4e03436073b33ec3879282ecc0
xuzhenxing <xuzhenxing@rakwireless.com>
2020-06-29 10:03:23 +0800

    Cleanup for iap_config.h

--- IAP/inc/iap_config.h
+++ IAP/inc/iap_config.h
@@ -1,43 +1,48 @@
 #ifndef __IAP_CONFIG_H__
 #define __IAP_CONFIG_H__
-/* Define if use bkp save flag  -------------------------------*/
+
+/* Define if use bkp register save flag */
 #define USE_BKP_SAVE_FLAG     1
 
-/* Define the APP start address -------------------------------*/
+/* Define the APP start address */
 #define ApplicationAddress    0x8003000
 
-/* Output printer switch --------------------------------------*/
+/* Output printer switch */
 #define ENABLE_PUTSTR         1
 
-/* Bootloader command -----------------------------------------*/
+/* Bootloader command */
 #define CMD_UPDATE_STR        "update"
 #define CMD_UPLOAD_STR        "upload"
 #define CMD_ERASE_STR		  "erase"
 #define CMD_MENU_STR          "menu"
 #define CMD_RUNAPP_STR        "runapp"
 #define CMD_ERROR_STR         "error"
-#define CMD_DISWP_STR         "diswp"//禁止写保护
+#define CMD_DISWP_STR         "diswp" /* Disable write protect for internal flash */
 
-/* IAP command------------------------------------------------ */
+/* IAP command */
 #if (USE_BKP_SAVE_FLAG == 1)
   #define IAP_FLAG_ADDR    BKP_DR1
 #else
-  #define IAP_FLAG_ADDR   (uint32_t)(ApplicationAddress - 1024 * 2)//App区域和Bootloader区域共享信息的地址(暂定大小为2K)
+  /* App and bootloader area will share 2KB */
+  #define IAP_FLAG_ADDR   (uint32_t)(ApplicationAddress - 1024 * 2)
 #endif
 
 //#define IAP_FLASH_FLAG_ADDR   0x8002800
 
+/* Initial status value(blank mcu) */
 #if (USE_BKP_SAVE_FLAG == 1)
-  #define INIT_FLAG_DATA      0x0000  //默认标志的数据(空片子的情况)
+  #define INIT_FLAG_DATA      0x0000
 #else
-  #define INIT_FLAG_DATA      0xFFFF   //默认标志的数据(空片子的情况)
+  #define INIT_FLAG_DATA      0xFFFF
 #endif
-#define UPDATE_FLAG_DATA      0xEEEE   //下载标志的数据
-#define UPLOAD_FLAG_DATA      0xDDDD   //上传标志的数据
-#define ERASE_FLAG_DATA       0xCCCC   //擦除标志的数据
-#define APPRUN_FLAG_DATA      0x5A5A   //APP不需要做任何处理，直接运行状态
 
-/* Define the Flsah area size ---------------------------------*/  
+/* Customize flag data */
+#define UPDATE_FLAG_DATA      0xEEEE
+#define UPLOAD_FLAG_DATA      0xDDDD
+#define ERASE_FLAG_DATA       0xCCCC
+#define APPRUN_FLAG_DATA      0x5A5A
+
+/* Define the Flsah area size */
 #if defined (STM32F10X_MD) || defined (STM32F10X_MD_VL)
  #define PAGE_SIZE                         (0x400)    /* 1 Kbyte */
  #define FLASH_SIZE                        (0x20000)  /* 128 KBytes */
@@ -54,10 +59,10 @@
  #error "Please select first the STM32 device to be used (in stm32f10x.h)"    
 #endif
 
-/* Compute the FLASH upload image size --------------------------*/  
+/* Compute the FLASH upload image size */
 #define FLASH_IMAGE_SIZE                   (uint32_t) (FLASH_SIZE - (ApplicationAddress - 0x08000000))
 
-/* The maximum length of the command string -------------------*/
+/* The maximum length of the command string */
 #define CMD_STRING_SIZE       128
 
 #endif
